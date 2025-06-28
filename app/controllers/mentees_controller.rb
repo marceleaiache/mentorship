c2bd08e3e442f3bdf509e8123a2d60d16f0f63ee
beatrizmitre-c42 <beatriz.mitre@codeminer42.com>
2025-06-28 17:39:04 -0300

    rails girls

--- app/controllers/mentees_controller.rb
+++ app/controllers/mentees_controller.rb
@@ -1,9 +1,49 @@
 class MenteesController < ApplicationController
- def index
-   @mentees = Mentee.all
- end
+  before_action :set_mentee, only: %i[ show edit update destroy ]
 
- def new
-   @mentee = Mentee.new
- end
+  def index
+    @mentees = Mentee.all
+  end
+
+  def show
+  end
+
+  def new
+    @mentee = Mentee.new
+  end
+
+  def create
+    @mentee = Mentee.new(mentee_params)
+    if @mentee.save
+      redirect_to @mentee, notice: "Mentora criado com sucesso."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @mentee.update(mentee_params)
+      redirect_to @mentee, notice: "Mentorada atualizado com sucesso."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @mentee.destroy
+    redirect_to mentees_path, notice: "Mentorada removido com sucesso."
+  end
+
+  private
+
+  def set_mentee
+    @mentee = Mentee.find(params[:id])
+  end
+
+  def mentee_params
+    params.expect(mentee: [ :name, :email, :linkedin_url, :bio ])
+  end
 end
